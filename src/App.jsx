617093c0d6b23706e774c4db6d1f068107ee41b8
luke <luke.nadackapadam@gmail.com>
2024-01-04 20:36:15 -0600

    Set login.jsx as root

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Header } from "./Header";
 import { Route, Routes } from "react-router-dom";
 import ApodRoute from "./routes/ApodRoute";
+import LoginRoute from "./routes/LoginRoute";
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
+        <Route path="/" element={<LoginRoute />} />
         <Route path="/apod" element={<ApodRoute />} />
       </Routes>
     </div>
---
+++ src/routes/LoginRoute.jsx
@@ -0,0 +1,11 @@
+import { Login } from "../Login";
+
+const LoginRoute = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginRoute;
